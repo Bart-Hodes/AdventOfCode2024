e88b48f7eec79ab65f54245bda16f4dab2d81126
Hodes <b.hodes@student.tue.nl>
2024-12-01 08:37:32 +0100

    first commit

---
+++ Day1/part2.py
@@ -0,0 +1,16 @@
+f = open("input.txt","r")
+
+count = 0
+historianOneLocationList = []
+historianTwoLocationList = []
+for line in f:
+    split = line.strip("\n").split("   ")
+
+    historianOneLocationList.append(int(split[0]))
+    historianTwoLocationList.append(int(split[1]))
+
+
+for idx in range(len(historianOneLocationList)):
+    multipier = historianTwoLocationList.count(historianOneLocationList[idx])
+    count += multipier * historianOneLocationList[idx]
+print(count)
